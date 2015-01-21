8652163dd545ca33a88ab76fdcd65331912dae99
Longwen Chen <chenlongwen@gmail.com>
2015-01-21 15:18:42 +0800

    修改 settings.py，添加 bootstrap 。 添加 index views。

--- deployment/views.py
+++ deployment/views.py
@@ -1,3 +1,11 @@
+# -*- coding: utf-8 -*-
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def index(request):
+    """ 首页: 显示文章列表
+    """
+    text = '欢迎您！'
+    return render(request, 'deployment/index.html', locals())
--- devops/settings.py
+++ devops/settings.py
@@ -67,9 +67,9 @@ DATABASES = {
 # Internationalization
 # https://docs.djangoproject.com/en/1.7/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'zh-cn'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Shanghai'
 
 USE_I18N = True
 
@@ -82,7 +82,17 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = 'staticfiles'
+MEDIA_URL = '/media/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 TEMPLATE_DIRS = (
-    os.path.join(BASE_DIR,  'templates'),
+    os.path.join(BASE_DIR, 'templates'),
+)
+
+MEDIA_ROOT = (
+    os.path.join(BASE_DIR, 'media')
 )
