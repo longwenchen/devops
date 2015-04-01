4fc3af4982c546cb91ee06ecb6798f84c0583228
Longwen Chen <chenlongwen@gmail.com>
2015-04-01 15:52:08 +0800

    0401

--- deployment/views.py
+++ deployment/views.py
@@ -2,20 +2,28 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 import subprocess
+import json
 
 # Create your views here.
 
 
 def index(request):
-    """ 首页: 显示文章列表
-    """
-    text = '欢迎您！'
-    return render(request, 'deployment/index.html', locals())
+
+    text = request.POST.get("blogtitle", "")
+    response_data = dict()
+
+    response_data['result'] = 'Writing'
+    response_data['message'] = text
+
+    if request.is_ajax():
+        return HttpResponse(json.dumps(response_data), content_type="application/json")
+    else:
+        return render(request, 'deployment/index.html', locals())
 
 
 def push(request):
     # if request.POST:
-    proc = subprocess.Popen(["/bin/bash", "/home/appuser/push.sh"], shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    proc = subprocess.Popen("ipconfig", stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     info = proc.communicate()
     return HttpResponse(info)
 
--- devops/settings.py
+++ devops/settings.py
@@ -95,5 +95,5 @@ TEMPLATE_DIRS = (
 )
 
 MEDIA_ROOT = (
-    os.path.join(BASE_DIR, 'media')
+    os.path.join(BASE_DIR, 'media'),
 )
