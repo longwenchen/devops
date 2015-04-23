d53c2459cbdc796bd12846771c390c2ffa2984dc
Longwen Chen <chenlongwen@gmail.com>
2015-04-24 01:17:20 +0800

    djcelery

--- devops/settings.py
+++ devops/settings.py
@@ -10,6 +10,8 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+import djcelery
+djcelery.setup_loader()
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 
@@ -36,6 +38,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'djcelery',
     'deployment',
 )
 
